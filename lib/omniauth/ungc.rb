96a5d86ebcd2f7964aeff0f5b95af9e2f97021d0
Benjamin Moss <drteeth@gmail.com>
2016-12-09 21:54:43 -0500

    new gem

---
+++ lib/omniauth/ungc.rb
@@ -0,0 +1,7 @@
+require "omniauth/ungc/version"
+
+module Omniauth
+  module Ungc
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/ungc/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Ungc
+    VERSION = "0.1.0"
+  end
+end
